15719a86bf8628af3f6b50f0e9803809f3706ced
Scott Gale <scottdgale@gmail.com>
2018-11-19 11:35:15 -0700

    Updated IntelliJ Files

---
+++ Code/src/Main.java
@@ -0,0 +1,13 @@
+import java.util.ArrayList;
+
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        System.out.println("Create the Graph");
+        Graph myGraph = new Graph(100,5,1000);
+        System.out.println(myGraph.toString());
+
+    }
+}
